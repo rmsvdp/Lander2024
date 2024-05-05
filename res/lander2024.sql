e715221108ddd37a52200f3bbd6448634e182c55
rmsvdp <rmsvdp2022@gmail.com>
2024-05-06 01:47:22 +0200

    codificando base de datos wip 1

--- res/lander2024.sql
+++ res/lander2024.sql
@@ -17,13 +17,13 @@ estado tinyint default 0  -- 0 no esta jugando y 1 esta jugando
 -- Lander
 create table lander(
 id_lander int auto_increment primary key,
-nombre varchar(32) not null primary key,
+nombre varchar(32) not null,
 t_a float default 0, -- tren de ate
 fuel int not null -- deposito combustible
 );
 
 create table perfil_pot(
-id_perfil int primary key auto_increment,
+id_perfil int primary key ,
 nivel int not null,
 valor int not null
 );
@@ -50,6 +50,8 @@ id_escenario int not null,
 fecha date not null
 );
 
+
+
 create table datos_sim(
 tiempo int not null,
 vel float not null,
@@ -64,3 +66,56 @@ tiempo int not null,
 fuel float not null,
 fecha timestamp);
 
+-- CONTRIBUCIONES AL DESARROLLO
+create table contrib(
+name VARCHAR(64),
+phase VARCHAR(64)
+);
+
+-- ----------- CLAVES FORÁNEAS -----------
+
+ALTER TABLE simulacion ADD FOREIGN KEY (id_usuario)   references usuario(id_usuario);
+ALTER TABLE simulacion ADD FOREIGN KEY (id_lander)    references lander(id_lander);
+ALTER TABLE simulacion ADD FOREIGN KEY (id_escenario) references escenario(id_escenario);
+ALTER TABLE puntuacion ADD FOREIGN KEY (id_usuario)   references usuario(id_usuario);
+ALTER TABLE confg_pot  ADD FOREIGN KEY (id_la)        references lander(id_lander);
+ALTER TABLE confg_pot  ADD FOREIGN KEY (id_perfil) references perfil_pot(id_perfil);
+ALTER TABLE datos_sim  ADD FOREIGN KEY  (id_sim)      references simulacion(id_sim);
+
+-- FUNCIONES Y PROCEDIMIENTOS
+
+CREATE DEFINER=`root`@`%` FUNCTION `check_access`(_NICK varchar(8), _PWD varchar(16)) RETURNS int
+    DETERMINISTIC
+BEGIN
+	DECLARE ID int DEFAULT -1;		-- Usuario no encontrado
+    DECLARE FECHA date;
+    DECLARE NC int;
+    DECLARE _ESTADO int DEFAULT 0;	-- Usuario no está jugando
+
+    SELECT COUNT(*) INTO ID FROM usuario
+            WHERE nick = _NICK AND pwd = _PWD;
+
+	IF ID=1 THEN 	-- Usuario encontrado
+    call debug("Encontrado, hago select");
+		SELECT id_usuario, fechaUC, num_com,estado INTO ID,FECHA, NC, _ESTADO
+		FROM usuario
+        WHERE nick = _NICK AND pwd = _PWD;
+
+		IF _ESTADO = 0 THEN 
+            call debug(concat("Entro con fecha =",FECHA));
+			IF FECHA <> current_date() THEN
+
+				UPDATE usuario  SET fechaUC = current_date(),
+								num_com = 1, estado = 1
+						WHERE id_usuario = ID; -- Actualizar la fecha de conexión
+			ELSE	-- Es el mismo día, comprobar numero de conexiones
+				IF NC > 3 THEN 
+					SET ID = -2 ; -- Superado el límite de conexiones diarias
+				END IF;
+			END IF;
+		ELSE
+            SET ID= -1;   -- Usuario existe pero está jugando
+		END IF;  -- _ESTADO = 0   
+    END IF;
+RETURN ID;
+END
